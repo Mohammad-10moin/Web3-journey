c6dfa02513c6cc87ffc5406f77bc09a8157091a7
Mohammad Moeenuddin <moinuddin10code@gmail.com>
2025-05-26 19:01:28 +0530

    hash that starts with given input

--- Day1/index.js
+++ Day1/index.js
@@ -1,6 +1,19 @@
 const crypto =require('crypto');
 
-const input="Moin";
+// const input="Moin";
 
-const hash=crypto.createHash('sha256').update(input).digest('hex');
-console.log(hash);
+// const hash=crypto.createHash('sha256').update(input).digest('hex');
+// console.log(hash);
+
+let input=0;
+while(true){
+    inputString=input.toString();
+    console.log(inputString);
+    const hash=crypto.createHash('sha256').update(inputString).digest('hex');
+    if(hash.startsWith("101103")){
+        console.log(hash);
+        console.log(inputString);
+        break;
+    }
+    input++;
+}
